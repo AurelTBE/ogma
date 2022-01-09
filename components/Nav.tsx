d5bfecac0805fa6e20eaeb6c35174bdcf11569f7
AurelTBE <aureltrouble@gmail.com>
2022-01-09 10:56:50 +0100

    Fixed UI 0.1

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import Link from 'next/link'
 
 function Nav() {
   return (
-    <div className="w-full">
-      <nav className="glassnav">
+    <div className="flex">
+      <nav className="w-full glassnav py-5 pl-4">
         <Link href="/">
             <a
             className="hover:bg-gray-700 hover:text-white text-stone-800 px-3 py-2 rounded-md text-sm font-medium"
             >
-            Accueil
+                Accueil
             </a>
-            </Link>
-            <Link href="/actus">
+        </Link>
+        <Link href="/actus">
             <a
             className="text-stone-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >
-            Actus
+                Actus
             </a>
-            </Link>
-            <Link href="/">
+        </Link>
+        <Link href="/">
             <a
             href="#"
             className="text-stone-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >
-            Projects
+                Projets
             </a>
-            </Link>
+        </Link>
       </nav>
     </div>
   );
