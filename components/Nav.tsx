44e56e4e83169b1a290d497a1d6440450bfa2441
AurelTBE <aureltrouble@gmail.com>
2022-01-10 00:45:14 +0100

    Nav style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,26 +9,25 @@ function Nav() {
     <div className="flex">
       <nav className="w-full neuenav py-5 pl-4">
         <Link href="/">
-            <a
-            className={`${activePage} hover:bg-gray-700 hover:text-white text-stone-800 px-3 py-2 rounded-md text-sm font-medium`}
-            >
-                Accueil
-            </a>
+          <a
+            className={`${activePage == "Accueil" && "activeneue" } text-stone-800 hover:bg-gray-700 hover:text-white px-3 mx-3 py-2 rounded-md text-sm font-medium`}
+          >
+            Accueil
+          </a>
         </Link>
         <Link href="/actus">
-            <a
-            className={`${activePage} text-stone-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
-            >
-                Actus
-            </a>
+          <a
+            className={`${activePage == "Actus" && "activeneue" } text-stone-800 hover:bg-gray-700 hover:text-white px-3 mx-3 py-2 rounded-md text-sm font-medium`}
+          >
+            Actus
+          </a>
         </Link>
         <Link href="/projets">
-            <a
-            href="#"
-            className={`${activePage} "text-stone-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
-            >
-                Projets
-            </a>
+          <a
+            className={`${activePage == "Projets" && "activeneue" } "text-stone-800 hover:bg-gray-700 hover:text-white px-3 mx-3 py-2 rounded-md text-sm font-medium`}
+          >
+            Projets
+          </a>
         </Link>
       </nav>
     </div>
